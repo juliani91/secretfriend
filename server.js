99dec3b816aabb8e648c7abfdd2a17c9caff56ef
juliani91 <juliani91@hotmail.com>
2015-03-30 14:39:00 -0500

    Checking domain name

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ pass: "wero2791"
 app.get('/',function(req,res){
 res.sendfile('index.html');
 });
-app.get('/send',function(req,res){
+app.get('/',function(req,res){
 //code to send e-mail.
 //Will be shown soon.
 
