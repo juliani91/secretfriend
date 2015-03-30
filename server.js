ff025aab041b501950804a71bd30b800d20c7f11
juliani91 <juliani91@hotmail.com>
2015-03-30 14:58:50 -0500

    Checking domain name

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/send',function(req,res){
 
 var mailOptions={
 to : req.query.to,
-subject : req.query.arraySubject[0].toString(),
+subject : req.query.arraySubject,
 //text : req.query.text
 }
 //console.log(mailOptions);
@@ -34,7 +34,7 @@ res.end("sent");
 //////// second field
 var mailOptions2={
 to : req.query.to2,
-subject : req.query.arraySubject[1].toString(),
+subject : req.query.arraySubject,
 //text : req.query.text
 }
 console.log(mailOptions2);
@@ -51,7 +51,7 @@ res.end("sent");
 //////// third field
 var mailOptions3={
 to : req.query.to3,
-subject : req.query.arraySubject[2].toString(),
+subject : req.query.arraySubject,
 //text : req.query.text
 }
 console.log(mailOptions3);
@@ -68,7 +68,7 @@ res.end("sent");
 //////// fourth field
 var mailOptions4={
 to : req.query.to4,
-subject : req.query.arraySubject[3].toString(),
+subject : req.query.arraySubject,
 //text : req.query.text
 }
 console.log(mailOptions4);
