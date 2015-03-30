0f6bd68d53c5b15be993f5dde83b739557d3051d
juliani91 <juliani91@hotmail.com>
2015-03-30 13:02:52 -0500

    Checking domain name

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ res.end("sent");
 
 });
 
-var ip  = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ip  = process.env.OPENSHIFT_NODEJS_IP;// || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT;// || 3000;
 
 app.listen(port, ip);
