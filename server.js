0d5043215f5da4d52c997b6a7a30a2068b43d7e4
juliani91 <juliani91@hotmail.com>
2015-04-01 15:30:51 -0500

    Checking style sheets

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ pass: "wero2791"
 app.get('/',function(req,res){
 res.sendfile('index.html');
 });
-app.use(express.static(__dirname + '/'));
-//app.use(express.static(__dirname + '/public/css'));
+
+app.use(express.static(__dirname + '/public'));
 app.get('/send',function(req,res){
 //code to send e-mail.
 //Will be shown soon.
